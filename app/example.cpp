071c23f8bd0aa36353cfcfbf5b1333d0d774c931
Matheus Ryan <mrysn@ic.ufal.br>
2024-06-21 23:58:49 -0300

    [build]: change the heuristic to Constraint Programming

--- app/example.cpp
+++ app/example.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
+#include "ortools/sat/cp_model.h"
  
 using namespace std;
 const int N1 = (int)2e5 + 100;
@@ -46,17 +47,99 @@ const long long M = 1e9+7;
 #define lli unsigned long long int
 #define ll long long
 
+using namespace operations_research;
+using namespace sat;
+using namespace std;
+
+map<string, pair<vector<string>, vector<int>>> GetData(const string& arquivo) {
+    ifstream infile(arquivo);
+    map<string, pair<vector<string>, vector<int>>> dados;
+    string linha;
+    while (getline(infile, linha)) {
+        istringstream iss(linha);
+        string professor, disciplina;
+        int horario;
+        iss >> professor;
+        vector<string> disciplinas;
+        vector<int> horarios_disponiveis;
+        while (iss >> disciplina >> horario) {
+            disciplinas.push_back(disciplina);
+            horarios_disponiveis.push_back(horario);
+        }
+        dados[professor] = make_pair(disciplinas, horarios_disponiveis);
+    }
+    return dados;
+}
 signed main(){
         	
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int teatchers;
+    map<string, pair<vector<string>, vector<int>>> dados_professores = GetData("ex01.txt");
 
-    cin >> teatchers;
+    map<string, vector<string>> turmas = {
+        {"T1", {"Mat1", "Mat2", "Mat3"}},
+        {"T2", {"Mat1", "Mat4", "Mat5"}},
+        {"T3", {"Mat2", "Mat3", "Mat5"}}
+    };
 
-    vvi graph(teatchers+1);
+    CpModelBuilder model;
 
-    cout << "Valor inicial de vértices: " << teatchers << nl
-    cout << "Valor inicial de cores: " << "NULL" << nl
+    map<tuple<string, string, int>, BoolVar> x;
+    for (const auto& turma : turmas) {
+        for (const auto& materia : turma.second) {
+            for (int horario = 1; horario <= 3; ++horario) {
+                x[{turma.first, materia, horario}] = model.NewBoolVar();
+            }
+        }
+    }
+
+    for (const auto& [professor, disciplinas_horarios] : dados_professores) {
+        const auto& [disciplinas, horarios_disponiveis] = disciplinas_horarios;
+        for (size_t i = 0; i < disciplinas.size(); ++i) {
+            const auto& disciplina = disciplinas[i];
+            const auto& horarios = horarios_disponiveis[i];
+            for (const auto& turma : turmas) {
+                if (find(turma.second.begin(), turma.second.end(), disciplina) != turma.second.end()) {
+                    LinearExpr sum;
+                    for (int horario : horarios) {
+                        sum += x[{turma.first, disciplina, horario}];
+                    }
+                    model.AddEquality(sum, 1);
+                }
+            }
+        }
+    }
+
+    for (const auto& turma : turmas) {
+        for (int horario = 1; horario <= 3; ++horario) {
+            LinearExpr sum;
+            for (const auto& materia : turma.second) {
+                sum += x[{turma.first, materia, horario}];
+            }
+            model.AddLessOrEqual(sum, 1);
+        }
+    }
+
+    CpSolver solver;
+    const CpSolverResponse response = solver.Solve(model.Build());
+
+    if (response.status() == CpSolverStatus::OPTIMAL || response.status() == CpSolverStatus::FEASIBLE) {
+        for (const auto& turma : turmas) {
+            cout << "Turma " << turma.first << ":\n";
+            for (const auto& materia : turma.second) {
+                for (int horario = 1; horario <= 3; ++horario) {
+                    if (solver.Value(x[{turma.first, materia, horario}])) {
+                        cout << "  Matéria " << materia << ": Horário " << horario << "\n";
+                    }
+                }
+            }
+        }
+    } else {
+        cout << "Nenhuma solução encontrada.\n";
+    }
+
+    return 0;
+
+
 }
